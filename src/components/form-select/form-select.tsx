8991a824602a937f92a0c60d5e9f529aca60d61c
k5va <k5va@yandex.ru>
2022-11-14 12:48:14 +0500

    Fixed FormSelect and FormTextField lables

--- src/components/form-select/form-select.tsx
+++ src/components/form-select/form-select.tsx
@@ -1,24 +1,28 @@
 import { Controller } from 'react-hook-form';
 import { InputLabel, Select } from '@mui/material';
 import { FormSelectProps } from './types';
+import { useState } from 'react';
 
 function FormSelect({ name, label, control, validator, children }: FormSelectProps): JSX.Element {
+  const [isFocused, setFocused] = useState(false);
+
   return (
     <Controller
         control={control}
         name={name}
         rules={validator}
         render={({
-          field: { onChange, onBlur, value, name, ref },
-          fieldState: { error },
+          field: {onChange, value, name, ref},
+          fieldState: {error},
         }) => (
           <>
-            <InputLabel htmlFor={name}>{label}</InputLabel>
+            <InputLabel error={!!error} focused={isFocused} htmlFor={name}>{label}</InputLabel>
             <Select
               id={name}
               value={value}
               onChange={onChange}
-              onBlur={onBlur}
+              onBlur={() => setFocused(false)}
+              onFocus={() => setFocused(true)}
               inputRef={ref}
               error={!!error}
             >
--- src/components/form-text-field/form-text-field.tsx
+++ src/components/form-text-field/form-text-field.tsx
@@ -1,28 +1,32 @@
-import {  Control, Controller, FieldValues } from 'react-hook-form';
+import { Control, Controller, FieldValues } from 'react-hook-form';
 import { InputLabel, TextField } from '@mui/material';
 import { FormTextFieldProps } from './types';
+import { useState } from 'react';
 
 function FormTextField<T extends FieldValues>({ name, label, control, validator }: FormTextFieldProps<T>): JSX.Element {
+  const [isFocused, setFocused] = useState(false); // TODO: is it OK
+
   return (
     <Controller
         name={name.toString()}
         control={control as Control} // TODO: Why needs type assertion?
         rules={validator}
         render={({
-          field: { onChange, onBlur, value, name, ref },
-          fieldState: { error },
+          field: {onChange, value, name, ref},
+          fieldState: {error},
         }) => (
           <>
-            <InputLabel htmlFor={name}>{label}</InputLabel>
+            <InputLabel error={!!error} focused={isFocused} htmlFor={name}>{label}</InputLabel>
             <TextField
               id={name}
               name={name}
               value={value}
               onChange={onChange}
-              onBlur={onBlur}
+              onBlur={() => setFocused(false)}
               inputRef={ref}
               error={!!error}
               helperText={error?.message}
+              onFocus={() => setFocused(true)}
             />
           </>
         )}
