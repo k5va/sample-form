0659454fd3ae9dc606ba26e046577a6f2e754aae
k5va <k5va@yandex.ru>
2022-11-15 21:54:41 +0500

    Changed Controller to useController

--- src/components/form-select/form-select.tsx
+++ src/components/form-select/form-select.tsx
@@ -1,38 +1,32 @@
-import { Controller } from 'react-hook-form';
+import { useController } from 'react-hook-form';
 import { FormControl, InputLabel, Select } from '@mui/material';
 import { FormSelectProps } from './types';
 
 function FormSelect({ name, label, control, validator, children, className }: FormSelectProps): JSX.Element {
   const labelId = `${label}-label`;
-  
+  const {
+    field: {onChange, onBlur, value, ref},
+    fieldState: {error}
+  } = useController({name, control, rules: validator});
+
   return (
-    <Controller
-        control={control}
-        name={name}
-        rules={validator}
-        render={({
-          field: {onChange, onBlur, value, name, ref},
-          fieldState: {error},
-        }) => (
-          <div className={className}>
-            <FormControl>
-              <InputLabel id={labelId}>{label}</InputLabel>
-              <Select
-                id={name}
-                value={value}
-                label={label}
-                labelId={labelId}
-                onChange={onChange}
-                onBlur={onBlur}
-                inputRef={ref}
-                error={!!error}
-              >
-                {children}
-              </Select>
-            </FormControl>
-          </div>
-        )}
-      />
+    <div className={className}>
+      <FormControl>
+        <InputLabel id={labelId}>{label}</InputLabel>
+        <Select
+          id={name}
+          value={value}
+          label={label}
+          labelId={labelId}
+          onChange={onChange}
+          onBlur={onBlur}
+          inputRef={ref}
+          error={!!error}
+        >
+          {children}
+        </Select>
+      </FormControl>
+    </div>
   );
 }
 
--- src/components/form-text-field/form-text-field.tsx
+++ src/components/form-text-field/form-text-field.tsx
@@ -1,32 +1,27 @@
-import { Controller, FieldValues } from 'react-hook-form';
+import { FieldValues, useController } from 'react-hook-form';
 import { TextField } from '@mui/material';
 import { FormTextFieldProps } from './types';
 
 function FormTextField<T extends FieldValues>({ name, label, control, validator, className }: FormTextFieldProps<T>): JSX.Element {
-  return ( // TODO: useControler
-    <Controller
+  const {
+    field: {onChange, onBlur, value, ref},
+    fieldState: {error}
+  } = useController({name, control, rules: validator});
+  
+  return (
+    <div className={className}>
+      <TextField
+        id={name}
         name={name}
-        control={control}
-        rules={validator}
-        render={({
-          field: {onChange, onBlur, value, name, ref},
-          fieldState: {error},
-        }) => (
-          <div className={className}>
-            <TextField
-              id={name}
-              name={name}
-              value={value}
-              label={label}
-              onChange={onChange}
-              onBlur={onBlur}
-              inputRef={ref}
-              error={!!error}
-              helperText={error?.message}
-            />
-          </div>
-        )}
+        value={value}
+        label={label}
+        onChange={onChange}
+        onBlur={onBlur}
+        inputRef={ref}
+        error={!!error}
+        helperText={error?.message}
       />
+    </div>
   );
 }
 
