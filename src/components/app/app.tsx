5600dae5448a58cb3ee5e1b2ac97639f3a6bed5f
k5va <k5va@yandex.ru>
2022-11-17 17:22:24 +0500

    Removed etra files

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from '@mui/material';
 import { Main } from '../../pages';
 import { theme } from '../../theme/theme';
-import './app.scss';
 
 function App(): JSX.Element {
   return (
